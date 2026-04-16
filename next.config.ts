87e890939cca10d92f96595ad617303915529b1f
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2026-04-16 13:03:32 +0100

    feat: add HeroSection component and update image quality configurations

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    qualities: [100, 75],
+    qualities: [100, 70, 75],
   },
   allowedDevOrigins: ["untagged-strife-starch.ngrok-free.dev"],
 };
